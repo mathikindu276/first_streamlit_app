bd3540a0b401442d63b7112a63c806304ea5cbc9
mathikindu276 <115666979+mathikindu276@users.noreply.github.com>
2022-10-17 21:52:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("use warehouse pc_rivery_wh")
 my_cur.execute("Select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
